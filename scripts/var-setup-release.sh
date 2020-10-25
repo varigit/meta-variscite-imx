57987f4aaa9db764844a5bed5e3a42a8e9e71029
Felix Radensky <felix.r@variscite.com>
2020-10-25 15:41:27 +0200

    var-setup-release.sh: Use debian packaging and include package management in images

--- scripts/var-setup-release.sh
+++ scripts/var-setup-release.sh
@@ -142,13 +142,16 @@ else
     cp $BUILD_DIR/conf/local.conf.org $BUILD_DIR/conf/local.conf
 fi
 
+echo >> conf/local.conf
+echo "# Switch to Debian packaging and include package-management in the image" >> conf/local.conf
+echo "PACKAGE_CLASSES = \"package_deb\"" >> conf/local.conf
+echo "EXTRA_IMAGE_FEATURES += \"package-management\"" >> conf/local.conf
 
 if [ ! -e $BUILD_DIR/conf/bblayers.conf.org ]; then
     cp $BUILD_DIR/conf/bblayers.conf $BUILD_DIR/conf/bblayers.conf.org
 else
     cp $BUILD_DIR/conf/bblayers.conf.org $BUILD_DIR/conf/bblayers.conf
 fi
-
 
 META_FSL_BSP_RELEASE="${CWD}/sources/meta-imx/meta-bsp"
 
