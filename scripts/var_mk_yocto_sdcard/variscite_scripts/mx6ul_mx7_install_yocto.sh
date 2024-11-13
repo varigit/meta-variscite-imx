1c45d669ae6f028acc351f66d542fe5d262a5136
Alexander Herfurtner <alexander.h@variscite.com>
2024-11-13 17:29:43 +0100

    mx6ul_mx7_install_yocto: add support for codec selection
    Beginning with DART-6UL v1.3, DART-6UL-5G v2.1 the codec wm8731 has been
    replaced with the codec wm8904.
    
    For eMMC installation this patch adds the new device trees to support the
    legacy codec wm8731.
    
    For NAND installation we can only install a single device tree. Add support
    to select the right one based on what can be detected at runtime.
    
    The runtime selection relays on trying to detect the codec using the i2c bus.
    
    If there is no codec at the expected address, it will fallback to the default
    device tree.
    
    If there is a codec, and it's the legacy one (wm8731), it will consider using
    the device tree supporting this codec. When it's the new codec, just use the
    default device tree name.
    
    Inherit from:
    varigit/meta-variscite-fslc@02ad1a09602b52c58652bf8254025a65cd0691c9 mx6ul_mx7_install_yocto: add support for codec selection
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- scripts/var_mk_yocto_sdcard/variscite_scripts/mx6ul_mx7_install_yocto.sh
+++ scripts/var_mk_yocto_sdcard/variscite_scripts/mx6ul_mx7_install_yocto.sh
@@ -42,6 +42,35 @@ check_images()
 	fi
 }
 
+# Detect if the SOM has a codec
+detect_codec_is_available()
+{
+	# A codec chip is expected at i2c address 0x1a
+	return $(i2cdetect -y 1 0x1a 0x1a | grep -iq -e 'uu' -e '1a')
+}
+
+# Detect the codec chip
+detect_codec_chip()
+{
+	if detect_codec_is_available; then
+		# Read codec chip ID
+		id=$(i2cget -f -y 1 0x1a 0x0 w 2>/dev/null)
+
+		if [ $? -eq 2 ]; then
+			echo "wm8731" # expected error (write-only device)
+		else
+			# Check codec chip ID
+			if [ "$id" == "0x0489" ]; then
+				echo "wm8904"
+			else
+				echo "n/a"
+			fi
+		fi
+	else
+		echo "n/a"
+	fi
+}
+
 install_bootloader_to_nand()
 {
 	echo
@@ -191,6 +220,8 @@ finish()
 blue_underlined_bold_echo "*** Variscite MX6UL/MX6ULL/MX7 Yocto eMMC/NAND Recovery ***"
 echo
 
+CODEC=`detect_codec_chip`
+
 while getopts :b:r:v: OPTION;
 do
 	case $OPTION in
@@ -290,6 +321,10 @@ if [[ $STORAGE_DEV == "nand" ]] ; then
 				KERNEL_DTB=imx6ull-var-dart-sd_nand.dtb
 			fi
 		fi
+
+		if [[ -n $CODEC && $CODEC == "wm8731" ]]; then
+			KERNEL_DTB="${KERNEL_DTB%.dtb}-${CODEC}.dtb"
+		fi
 	elif [[ $BOARD == "mx7" ]] ; then
 		KERNEL_DTB=imx7d-var-som-nand.dtb
 	fi
@@ -316,12 +351,18 @@ elif [[ $STORAGE_DEV == "emmc" ]] ; then
 		if [[ $BOARD == "mx6ul" || $BOARD == "mx6ul5g" ]] ; then
 			KERNEL_DTBS="imx6ul-var-dart-emmc_wifi.dtb
 				     imx6ul-var-dart-5g-emmc_wifi.dtb
-				     imx6ul-var-dart-sd_emmc.dtb"
+				     imx6ul-var-dart-sd_emmc.dtb
+				     imx6ul-var-dart-emmc_wifi-wm8731.dtb
+				     imx6ul-var-dart-5g-emmc_wifi-wm8731.dtb
+				     imx6ul-var-dart-sd_emmc-wm8731.dtb"
 			FAT_VOLNAME=BOOT-VAR6UL
 		elif [[ $BOARD == "mx6ull" || $BOARD == "mx6ull5g"  ]] ; then
 			KERNEL_DTBS="imx6ull-var-dart-emmc_wifi.dtb
 				     imx6ull-var-dart-5g-emmc_wifi.dtb
-				     imx6ull-var-dart-sd_emmc.dtb"
+				     imx6ull-var-dart-sd_emmc.dtb
+				     imx6ull-var-dart-emmc_wifi-wm8731.dtb
+				     imx6ull-var-dart-5g-emmc_wifi-wm8731.dtb
+				     imx6ull-var-dart-sd_emmc-wm8731.dtb"
 			FAT_VOLNAME=BOOT-VAR6ULL
 		fi
 	elif [[ $BOARD == "mx7" ]] ; then
